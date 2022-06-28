7bfa2bee2ed1347ef6038b8bef0b127765582d96
afikanzoro <muchemimutahi@gmail.com>
2022-06-28 18:30:10 +0300

    Create and style a calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+/* eslint-disable-next-line react/prefer-stateless-function */
+export class Calculator extends Component {
+  /* constructor(props) {
+    super(props);
+  }
+  */
+
+  render() {
+    return (
+      <div className="Calculator">
+        <div className="display">
+          <span>0</span>
+        </div>
+        <div className="operators">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button className="orange" type="button">+</button>
+        </div>
+        <div className="first-digits">
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="orange">*</button>
+        </div>
+        <div className="second-digits">
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="orange">-</button>
+        </div>
+        <div className="third-digits">
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="orange">-</button>
+        </div>
+        <div className="bottom-digits">
+          <button type="button" className="wide-button">0</button>
+          <button type="button">.</button>
+          <button type="button" className="orange">=</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
