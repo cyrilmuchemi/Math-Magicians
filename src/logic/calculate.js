1f97ced90d9feafad40b4cd0212f7d127c0f38dc
afikanzoro <muchemimutahi@gmail.com>
2022-06-29 18:06:43 +0300

    Remove comments of files

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,15 +4,6 @@ function isNumber(item) {
   return !!item.match(/[0-9]+/);
 }
 
-/**
- * Given a button name and a calculator data object, return an updated
- * calculator data object.
- *
- * Calculator data object contains:
- *   total:s      the running total
- *   next:String       the next number to be operated on with the total
- *   operation:String  +, -, etc.
- */
 export default function calculate(obj, buttonName) {
   if (buttonName === 'AC') {
     return {
@@ -26,14 +17,14 @@ export default function calculate(obj, buttonName) {
     if (buttonName === '0' && obj.next === '0') {
       return {};
     }
-    // If there is an operation, update next
+  
     if (obj.operation) {
       if (obj.next && obj.next !== '0') {
         return { ...obj, next: obj.next + buttonName };
       }
       return { ...obj, next: buttonName };
     }
-    // If there is no operation, update next and clear the value
+  
     if (obj.next && obj.next !== '0') {
       return {
         next: obj.next + buttonName,
