e9cb5d270d2667d1197488563c75010d4618761b
afikanzoro <muchemimutahi@gmail.com>
2022-07-01 09:28:49 +0300

    Refactor the calculator component to be a functional component insead of hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+export const Calculator = () => {
+  const importedItems = {
+    total: 0,
+    next: null,
+    operation: null,
+  };
 
-  calculationBtn = (x) => {
-    this.setState((s) => calculate(s, x));
-  }
+  const [text, setText] = useState(importedItems);
 
-  render() {
-    const { next, operation, total } = this.state;
-    return (
+  const calculationBtn = (x) => {
+    setText(() => calculate(text, x));
+  };
+
+  const { total, next, operation } = text;
+
+  return (
+    <div>
       <div className="Calculator">
         <div className="display">
           <span>{total}</span>
@@ -25,37 +25,37 @@ export class Calculator extends React.PureComponent {
           <span>{next}</span>
         </div>
         <div className="operators">
-          <button type="button" onClick={() => this.calculationBtn('AC')}>AC</button>
-          <button type="button" onClick={() => this.calculationBtn('+/-')}>+/-</button>
-          <button type="button" onClick={() => this.calculationBtn('%')}>%</button>
-          <button className="orange" type="button" onClick={() => this.calculationBtn('÷')}>÷</button>
+          <button type="button" onClick={() => calculationBtn('AC')}>AC</button>
+          <button type="button" onClick={() => calculationBtn('+/-')}>+/-</button>
+          <button type="button" onClick={() => calculationBtn('%')}>%</button>
+          <button className="orange" type="button" onClick={() => calculationBtn('÷')}>÷</button>
         </div>
         <div className="first-digits">
-          <button type="button" onClick={() => this.calculationBtn('7')}>7</button>
-          <button type="button" onClick={() => this.calculationBtn('8')}>8</button>
-          <button type="button" onClick={() => this.calculationBtn('9')}>9</button>
-          <button type="button" className="orange" onClick={() => this.calculationBtn('x')}>x</button>
+          <button type="button" onClick={() => calculationBtn('7')}>7</button>
+          <button type="button" onClick={() => calculationBtn('8')}>8</button>
+          <button type="button" onClick={() => calculationBtn('9')}>9</button>
+          <button type="button" className="orange" onClick={() => calculationBtn('x')}>x</button>
         </div>
         <div className="second-digits">
-          <button type="button" onClick={() => this.calculationBtn('4')}>4</button>
-          <button type="button" onClick={() => this.calculationBtn('5')}>5</button>
-          <button type="button" onClick={() => this.calculationBtn('6')}>6</button>
-          <button type="button" className="orange" onClick={() => this.calculationBtn('+')}>+</button>
+          <button type="button" onClick={() => calculationBtn('4')}>4</button>
+          <button type="button" onClick={() => calculationBtn('5')}>5</button>
+          <button type="button" onClick={() => calculationBtn('6')}>6</button>
+          <button type="button" className="orange" onClick={() => calculationBtn('+')}>+</button>
         </div>
         <div className="third-digits">
-          <button type="button" onClick={() => this.calculationBtn('1')}>1</button>
-          <button type="button" onClick={() => this.calculationBtn('2')}>2</button>
-          <button type="button" onClick={() => this.calculationBtn('3')}>3</button>
-          <button type="button" className="orange" onClick={() => this.calculationBtn('-')}>-</button>
+          <button type="button" onClick={() => calculationBtn('1')}>1</button>
+          <button type="button" onClick={() => calculationBtn('2')}>2</button>
+          <button type="button" onClick={() => calculationBtn('3')}>3</button>
+          <button type="button" className="orange" onClick={() => calculationBtn('-')}>-</button>
         </div>
         <div className="bottom-digits">
-          <button type="button" className="wide-button" onClick={() => this.calculationBtn('0')}>0</button>
-          <button type="button" onClick={() => this.calculationBtn('.')}>.</button>
-          <button type="button" className="orange" onClick={() => this.calculationBtn('=')}>=</button>
+          <button type="button" className="wide-button" onClick={() => calculationBtn('0')}>0</button>
+          <button type="button" onClick={() => calculationBtn('.')}>.</button>
+          <button type="button" className="orange" onClick={() => calculationBtn('=')}>=</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
